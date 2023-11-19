1230689ea0498dfb5ef86b5cb54bfb989b477d55
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-19 22:17:21 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -121,7 +121,7 @@ echo "LAST_PORT is $LAST_PORT. Continue..."
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+chmod +x "${WORKDIR}"/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
