7e53b2b0d60b467d9c88149853e943fbd5e2459b
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-22 17:13:44 +0700

    Update v.sh

--- v.sh
+++ v.sh
@@ -128,6 +128,146 @@ rm -rf /root/3proxy-3proxy-0.8.6
 echo "Starting Proxy"
 
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+install_3proxy() {
+    echo "Installing 3proxy..."
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cd $WORKDIR
+}
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456 
+flush
+
+$(awk -F "/" '{print "\n" \
+"" $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "//$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+
+# Thêm vào rc.local để tự động chạy khi khởi động hệ thống
+add_to_rc_local() {
+    cat <<EOF >> /etc/rc.local
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
+EOF
+}
+
+# Thêm vào crontab để kiểm tra xoay tự động mỗi 10 phút
+add_rotation_cronjob() {
+    echo "*/10 * * * * bash ${WORKDIR}/rotate_proxies.sh" >> /etc/crontab
+}
+
+echo "Installing dependencies..."
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
+echo "Setting up working folder..."
+WORKDIR="/home/cloudfly"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal IP = ${IP4}. External IPv6 subnet = ${IP6}"
+
+while :; do
+  read -p "Enter FIRST_PORT (between 10000 and 60000): " FIRST_PORT
+  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
+    echo "OK! Valid number"
+    break
+  else
+    echo "Number out of range, try again"
+  fi
+done
+LAST_PORT=$(($FIRST_PORT + 3000))
+echo "LAST_PORT is $LAST_PORT. Continuing..."
+
+gen_data > $WORKDIR/data.txt
+gen_iptables > $WORKDIR/boot_iptables.sh
+gen_ifconfig > $WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh
+
+gen_3proxy > /usr/local/etc/3proxy/3proxy.cfg
+
+add_to_rc_local
+
+# Tạo script rotate_proxies.sh
+echo '#!/bin/bash' > ${WORKDIR}/rotate_proxies.sh
+echo 'new_ipv6=$(get_new_ipv6)' >> ${WORKDIR}/rotate_proxies.sh
+echo 'update_3proxy_config "$new_ipv6"' >> ${WORKDIR}/rotate_proxies.sh
+echo 'restart_3proxy' >> ${WORKDIR}/rotate_proxies.sh
+chmod +x ${WORKDIR}/rotate_proxies.sh
+
+# Thêm vào crontab để xoay tự động
+add_rotation_cronjob
+
+gen_proxy_file_for_user
+rm -rf /root/3proxy-3proxy-0.8.6
+echo "Starting Proxy"
+
+#!/bin/bash
 
 CONFIG_FILE="/usr/local/etc/app_config.conf"
 PROXY_CONFIG_FILE="/usr/local/etc/3proxy/3proxy.cfg"
@@ -171,16 +311,24 @@ apply_configuration_changes() {
 }
 
 enable_ip_authentication() {
-  echo "Enabling IP Authentication..."
+  echo "Đang Bật Xác Thực IP..."
+
+  read -p "Nhập ít nhất một địa chỉ IP để xác thực (sử dụng dấu phẩy để phân tách nếu có nhiều IPs): " ip_addresses
 
   if [ -f "$CONFIG_FILE" ]; then
-    sed -i 's/IP_AUTHENTICATION=false/IP_AUTHENTICATION=true/' "$CONFIG_FILE"
-    apply_configuration_changes
+    # Kiểm tra xem người dùng đã nhập ít nhất một địa chỉ IP hay không
+    if [ -n "$ip_addresses" ]; then
+      # Thay thế dòng cấu hình IP_AUTHENTICATION=false bằng IP_AUTHENTICATION=true và thêm danh sách IP cần xác thực
+      sed -i "s/IP_AUTHENTICATION=false/IP_AUTHENTICATION=true\nALLOWED_IPS=\"$ip_addresses\"/" "$CONFIG_FILE"
+      apply_configuration_changes
+      echo "Đã Bật Xác Thực IP thành công cho các địa chỉ: $ip_addresses."
+    else
+      echo "Lỗi: Ít nhất một địa chỉ IP là bắt buộc để Bật Xác Thực IP."
+    fi
   else
-    echo "Error: Configuration file not found."
+    echo "Lỗi: Không tìm thấy tệp cấu hình."
   fi
 
-  echo "IP Authentication enabled successfully."
   sleep 2
 }
 
@@ -201,8 +349,8 @@ disable_ip_authentication() {
 generate_new_ports() {
   echo "Generating New Ports..."
 
-  starting_port=50000
-  number_of_ports=1500
+  starting_port=10000
+  number_of_ports=5000
 
   for ((i = 0; i < number_of_ports; i++)); do
     new_port=$((starting_port + i))
@@ -272,11 +420,10 @@ update_3proxy_config() {
 }
 
 restart_3proxy() {
-  # Your logic to restart the 3proxy service
   systemctl restart 3proxy.service
 }
 
-# Main menu loop
+# Gọi menu quản lý 3Proxy
 while true; do
   display_menu
   menu_option
