26c10d63c63481d24593c2dd35669fe9be7b0cee
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-22 21:29:48 +0700

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,141 +1,3 @@
-m() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
-}
-
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
-gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
-
-install_3proxy() {
-    echo "Installing 3proxy..."
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cd $WORKDIR
-}
-
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456 
-flush
-
-$(awk -F "/" '{print "\n" \
-"" $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
-}
-
-gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
-EOF
-}
-
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
-    done
-}
-
-gen_iptables() {
-    cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
-cat >> /etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-EOF
-
-echo "Installing apps..."
-yum -y install wget gcc net-tools bsdtar zip >/dev/null
-rotate_script="${WORKDIR}/rotate_proxies.sh"
-echo '#!/bin/bash' > "$rotate_script"
-echo 'new_ipv6=$(get_new_ipv6)' >> "$rotate_script"
-echo 'update_3proxy_config "$new_ipv6"' >> "$rotate_script"
-echo 'restart_3proxy' >> "$rotate_script"
-chmod +x "$rotate_script"
-
-# Add rotation to crontab for automatic rotation
-add_rotation_cronjob() {
-    echo "*/10 * * * * $rotate_script" >> /etc/crontab
-}
-
-echo "Installing necessary packages..."
-yum -y install wget gcc net-tools bsdtar zip >/dev/null
-
-cat << EOF > /etc/rc.d/rc.local
-#!/bin/bash
-touch /var/lock/subsys/local
-EOF
-
-install_3proxy
-
-echo "Working folder = /home/cloudfly"
-WORKDIR="/home/cloudfly"
-WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
-
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-
-echo "Internal IP = ${IP4}. External IPv6 subnet = ${IP6}"
-
-while :; do
-  read -p "Enter FIRST_PORT (between 10000 and 60000): " FIRST_PORT
-  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
-    echo "OK! Valid number"
-    break
-  else
-    echo "Number out of range, try again"
-  fi
-done
-LAST_PORT=$(($FIRST_PORT + 3000))
-echo "LAST_PORT is $LAST_PORT. Continuing..."
-
-gen_data > $WORKDIR/data.txt
-gen_iptables > $WORKDIR/boot_iptables.sh
-gen_ifconfig > $WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
-
-gen_3proxy > /usr/local/etc/3proxy/3proxy.cfg
-
-bash /etc/rc.local
-
-gen_proxy_file_for_user
-rm -rf /root/3proxy-3proxy-0.8.6
-echo "Starting Proxy"
-
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
@@ -164,6 +26,16 @@ install_3proxy() {
     cd $WORKDIR
 }
 
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "//$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}
+}
+
 gen_3proxy() {
     cat <<EOF
 daemon
@@ -187,61 +59,35 @@ EOF
 }
 
 gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
-EOF
+    awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA} > proxy.txt
 }
 
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
-    done
-}
+rotate_script="${WORKDIR}/rotate_proxies.sh"
+echo '#!/bin/bash' > "$rotate_script"
+echo 'new_ipv6=$(get_new_ipv6)' >> "$rotate_script"
+echo 'update_3proxy_config "$new_ipv6"' >> "$rotate_script"
+echo 'restart_3proxy' >> "$rotate_script"
+chmod +x "$rotate_script"
 
-gen_iptables() {
-    cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
-# Thêm vào rc.local để tự động chạy khi khởi động hệ thống
-add_to_rc_local() {
-    cat <<EOF >> /etc/rc.local
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-EOF
-}
-
-# Thêm vào crontab để kiểm tra xoay tự động mỗi 10 phút
+# Add rotation to crontab for automatic rotation
 add_rotation_cronjob() {
-    echo "*/10 * * * * bash ${WORKDIR}/rotate_proxies.sh" >> /etc/crontab
+    echo "*/10 * * * * $rotate_script" >> /etc/crontab
 }
 
-echo "Installing dependencies..."
+echo "Installing necessary packages..."
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
+
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
 
 install_3proxy
 
-# Update these paths if needed
+echo "Working directory set to /home/cloudfly"
 WORKDIR="/home/cloudfly"
-IPTABLES_SCRIPT="${WORKDIR}/boot_iptables.sh"
-IFCONFIG_SCRIPT="${WORKDIR}/boot_ifconfig.sh"
-
-# Add the correct paths in rc.local
-cat >> /etc/rc.local <<EOF
-bash ${IPTABLES_SCRIPT}
-bash ${IFCONFIG_SCRIPT}
-ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-EOF
+WORKDATA="${WORKDIR}/data.txt"
+mkdir -p $WORKDIR && cd $_  # Use -p to avoid errors if the directory already exists
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -249,36 +95,26 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal IP = ${IP4}. External IPv6 subnet = ${IP6}"
 
 while :; do
-  read -p "Enter FIRST_PORT (between 10000 and 60000): " FIRST_PORT
-  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
-    echo "OK! Valid number"
-    break
-  else
-    echo "Number out of range, try again"
-  fi
+    read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
+    if [[ ! $FIRST_PORT =~ ^[0-9]+$ || $FIRST_PORT -lt 10000 || $FIRST_PORT -gt 60000 ]]; then
+        echo "Invalid input. Enter a valid number between 10000 and 60000."
+    else
+        echo "OK! Valid number"
+        break
+    fi
 done
+
 LAST_PORT=$(($FIRST_PORT + 3000))
 echo "LAST_PORT is $LAST_PORT. Continuing..."
 
-gen_data > $WORKDIR/data.txt
-gen_iptables > $WORKDIR/boot_iptables.sh
-gen_ifconfig > $WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh
+gen_data > ${WORKDIR}/data.txt
+gen_iptables > ${WORKDIR}/boot_iptables.sh
+
+gen_3proxy > /usr/local/etc/3proxy/3proxy.cfg
 
 gen_3proxy > /usr/local/etc/3proxy/3proxy.cfg
 
 add_to_rc_local
-
-# Tạo script rotate_proxies.sh
-echo '#!/bin/bash' > ${WORKDIR}/rotate_proxies.sh
-echo 'new_ipv6=$(get_new_ipv6)' >> ${WORKDIR}/rotate_proxies.sh
-echo 'update_3proxy_config "$new_ipv6"' >> ${WORKDIR}/rotate_proxies.sh
-echo 'restart_3proxy' >> ${WORKDIR}/rotate_proxies.sh
-chmod +x ${WORKDIR}/rotate_proxies.sh
-
-# Thêm vào crontab để xoay tự động
-add_rotation_cronjob
 
 gen_proxy_file_for_user
 rm -rf /root/3proxy-3proxy-0.8.6
@@ -296,6 +132,7 @@ display_menu() {
   echo "[1] Enable IP Authentication"
   echo "[2] Disable IP Authentication"
   echo "[3] Generate New Ports"
+  
   echo "[4] Enable Auto Rotation"
   echo "[5] Create and Download Proxies"
   echo "[6] Show Proxy List"
