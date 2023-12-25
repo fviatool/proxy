a67c272a3538b76af6199e0fa4c8acbf4e7693cf
FVIA <130783224+fviatool@users.noreply.github.com>
2023-12-26 03:05:02 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -24,37 +24,37 @@ install_3proxy() {
     #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
     #chmod +x /etc/init.d/3proxy
     #chkconfig 3proxy on
-    cd $WORKDIR
+    cd /root
 }
 
 gen_3proxy() {
     cat <<EOF
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456 
-flush
+    echo daemon
+    echo maxconn 3000
+    echo nserver 1.1.1.1
+    echo nserver [2606:4700:4700::1111]
+    echo nserver [2606:4700:4700::1001]
+    echo nserver [2001:4860:4860::8888]
+    echo nscache 65536
+    echo timeouts 1 5 30 60 180 1800 15 60
+    echo setgid 65535
+    echo setuid 65535
+    echo stacksize 6291456 
+    echo flush
+    echo auth none
 
 $(awk -F "/" '{print "\n" \
 "" $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+"flush\n"}' /root/data.txt)
 EOF
 }
 
 gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+    cat >/root/proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' /root/data.txt)
 EOF
 }
-
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
@@ -64,64 +64,22 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' /root/ipv6.txt) 
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' /root/ipv6.txt)
 EOF
 }
-
-cat << EOF > /etc/rc.d/rc.local
-#!/bin/bash
-touch /var/lock/subsys/local
-EOF
-
-rotate_proxy_script() {
-    cat <<EOF
-#!/bin/sh
-service 3proxy restart
-EOF
-}
-
-rotate_ipv6() {
-    echo "Rotating IPv6 addresses..."
-    new_ipv6=$(get_new_ipv6)
-    update_3proxy_config "$new_ipv6"
-    service 3proxy restart
-    echo "3proxy restarted successfully."
-    echo "IPv6 rotation completed."
-}
-
-get_new_ipv6() {
-    random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
-    echo "$random_ipv6"
-}
-
-update_3proxy_config() {
-    new_ipv6=$1
-    sed -i "s/old_ipv6_address/$new_ipv6/" /usr/local/etc/3proxy/3proxy.cfg
-}
-
-add_rotation_cronjob() {
-    echo "*/10 * * * * root ${WORKDIR}/rotate_proxies.sh" >> /etc/crontab
-    echo "Cronjob added for IPv6 rotation every 10 minutes."
-}
-
-# Tự động xoay proxy sau mỗi 10 phút
-(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_proxies.sh") | crontab -
 
 echo "installing apps"
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = /root/proxy"
-WORKDIR="/home/proxy"
-WORKDATA="${WORKDIR}/ipv6.txt"
-mkdir $WORKDIR && cd $_
+echo "working folder = /root"
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -138,19 +96,19 @@ while :; do
     echo "Number out of range, try again"
   fi
 done
-LAST_PORT=$(($FIRST_PORT + 5000))
+LAST_PORT=$(($FIRST_PORT + 5555))
 echo "LAST_PORT is $LAST_PORT. Continue..."
 
-gen_data >$WORKDIR/ipv6.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+gen_data >/root/data.txt
+gen_iptables >/root/boot_iptables.sh
+gen_ifconfig >/root/boot_ifconfig.sh
+chmod +x /root/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
+bash /root/boot_iptables.sh
+bash /root/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
