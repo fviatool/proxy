5197585efde8c38ba3c41286243aeaea9bfd981f
FVIA <130783224+fviatool@users.noreply.github.com>
2024-01-05 16:11:54 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -95,7 +95,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 while :; do
-  read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
+  read -p "Nhập 10000 Đến 60000: " FIRST_PORT
   [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
   if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
     echo "OK! Đợi Tí..."
