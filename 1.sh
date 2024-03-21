71d0267b47abf7ce88862bb92072345533642467
FVIA <130783224+fviatool@users.noreply.github.com>
2024-03-21 13:50:04 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Lấy địa chỉ IPv6 và gateway từ lệnh ip
+IPV6ADDR=$(ip -6 addr show dev eth0 | grep inet6 | awk '{print $2}' | grep -v '^fe80' | cut -d'/' -f1)
+IPV6_DEFAULTGW=$(ip -6 route show default | awk '/via/ {print $3}')
+
+# Thêm cấu hình IPv6 vào tệp cấu hình của giao diện mạng
+echo "IPV6_FAILURE_FATAL=no
+IPV6_ADDR_GEN_MODE=stable-privacy
+IPV6ADDR=$IPV6ADDR/64
+IPV6_DEFAULTGW=$IPV6_DEFAULTGW" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+# Khởi động lại dịch vụ mạng để áp dụng cấu hình mới
+service network restart
+
+# Cài đặt và cấu hình 3proxy
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    tr </dev/urandom -dc A-Za-z0-9 | head -c8
     echo
 }
 
@@ -15,16 +29,13 @@ gen64() {
 }
 
 install_3proxy() {
-    echo "Installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/bin
-    mkdir -p /usr/local/etc/3proxy/logs
-    mkdir -p /usr/local/etc/3proxy/stat
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    mkdir -p /usr/local/etc/3proxy/{bin,stat}
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ../init.d/3proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
@@ -33,70 +44,74 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 1000
-nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
 flush
-auth none
+log /dev/null
 
-$(awk -F "/" '{print "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5}' ${WORKDATA})
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong cache\n" \
+"allow "$1"\n" \
+"socks -n -a -s0 -64 -olSO_REUSEADDR,SO_REUSEPORT -ocTCP_TIMESTAMPS,TCP_NODELAY -osTCP_NODELAY -p"$4" -i"$3" -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
-    awk -F "/" '{print $3 ":" $4}' ${WORKDATA} > proxy.txt
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3":"$4":"$1":"$2}' ${WORKDATA})
+EOF
 }
 
 upload_proxy() {
-    zip -r proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-
-    echo "Proxy is ready! Format IP:PORT"
+    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
 }
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
-    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 " -m state --state NEW -j ACCEPT"}' ${WORKDATA}
+    cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport "$4"  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
 }
 
 gen_ifconfig() {
-    awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA}
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add "$5"/64"}' ${WORKDATA})
+EOF
 }
 
-rotate_ipv6() {
-    echo "Rotating IPv6"
-    ip -6 address flush dev eth0
-    sleep 5
-    systemctl restart network
-}
-
-echo "Installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "Working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir -p $WORKDIR && cd $_
+mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+echo "Your IPv4 address:"
+read IPv4
+echo "Your IPv6 subnet:"
+read IPv6
 
-echo "Internal IP = ${IP4}. External sub for IPv6 = ${IP6}"
+IP4=${IPv4}
+IP6=${IPv6}
 
-echo "How many proxies do you want to create? Example: 500"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create?"
 read COUNT
 
-FIRST_PORT=10000
+echo "Port starting proxies:"
+read START_PORT
+
+FIRST_PORT=${START_PORT}
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
@@ -106,21 +121,28 @@ chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
+echo "net.ipv6.conf.eth0.proxy_ndp=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv6.ip_nonlocal_bind=1" >> /etc/sysctl.conf
+echo "vm.max_map_count=95120" >> /etc/sysctl.conf
+echo "kernel.pid_max=95120" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range=1024 65000" >> /etc/sysctl.conf
+sudo sysctl -p
+
+chmod -R 777 /usr/local/etc/3proxy/
+
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
+
+ulimit -n 999999
+
 service 3proxy start
 EOF
 
 bash /etc/rc.local
 
 gen_proxy_file_for_user
-
 upload_proxy
-
-# Rotate IPv6 every 10 minutes
-while true; do
-    rotate_ipv6
-    sleep 600 # 10 minutes
-done
