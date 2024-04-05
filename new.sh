57177e7f91bc6045b183059c24a5d841e7244832
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-06 06:22:32 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -5,31 +5,32 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 ipv4Local="192.168.1.9"
 
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 install_3proxy() {
     echo "installing 3proxy"
+    apt-get update && apt-get install -y wget gcc bsdtar make >/dev/null
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    wget -qO- $URL | tar -xzvf -
+    cd 3proxy-3proxy-0.8.6 || exit 1
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    cd $WORKDIR
+    cd "$WORKDIR" || exit 1
 }
 
 download_proxy() {
-    cd $WORKDIR || exit 1
+    cd "$WORKDIR" || exit 1
     curl -F "proxy.txt" https://transfer.sh
 }
 
@@ -51,61 +52,58 @@ flush
 $(awk -F "/" '{print "auth iponly\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+"flush\n"}' "${WORKDATA}")
 EOF
 }
 
 gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(gen64 $IP6)" >> ${WORKDATA}ipv6.txt
-        echo "$IP4/$port:user$port:$(random)" >> ${WORKDATA}ipv4.txt
+    seq "$FIRST_PORT" "$LAST_PORT" | while read -r port; do
+        echo "$(gen64 "$IP6")" >> "${WORKDATA}ipv6.txt"
+        echo "$IP4/$port:user$port:$(random)" >> "${WORKDATA}ipv4.txt"
     done
 }
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' "${WORKDATA}") 
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' "${WORKDATA}")
 EOF
 }
 
 echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+apt-get update && apt-get install -y gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
 echo "working folder = /home/bkns"
 WORKDIR="/home/bkns"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir "$WORKDIR" && cd "$_" || exit 1
 
 # Lấy phần thứ 3 và thứ 4 của địa chỉ IPv4 để tạo địa chỉ IPv6
-IPC=$(echo $ipv4Local | cut -d"." -f3)
-IPD=$(echo $ipv4Local | cut -d"." -f4)
+IPC=$(echo "$ipv4Local" | cut -d"." -f3)
+IPD=$(echo "$ipv4Local" | cut -d"." -f4)
 
-# Gán giá trị cho biến $IP4
-IP4=$ipv4Local
-
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
 FIRST_PORT=10000
 LAST_PORT=12000
 
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+gen_data > "$WORKDIR/data.txt"
+gen_iptables > "$WORKDIR/boot_iptables.sh"
+gen_ifconfig > "$WORKDIR/boot_ifconfig.sh"
 chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
+bash "${WORKDIR}/boot_iptables.sh"
+bash "${WORKDIR}/boot_ifconfig.sh"
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
