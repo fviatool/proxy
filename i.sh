179d929f22dbe21ddc251ecd0eadbdb0dc0db1eb
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-06 06:46:06 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
 
-ipv4=$(curl -4 -s icanhazip.com)
-IPC=$(echo "$ipv4" | cut -d"." -f3)
-IPD=$(echo "$ipv4" | cut -d"." -f4)
-INT=$(ls /sys/class/net | grep e)
-
-if [ "$IPC" = "4" ]; then
-    IPV6_ADDRESS="2001:ee0:4f9b::$IPD:0000/64"
-    GATEWAY="2001:ee0:4f9b:92b0::1"
-elif [ "$IPC" = "5" ]; then
-    IPV6_ADDRESS="2001:ee0:4f9b::$IPD:0000/64"
-    GATEWAY="2001:ee0:4f9b:92b0::1"
-elif [ "$IPC" = "244" ]; then
-    IPV6_ADDRESS="2001:ee0:4f9b::$IPD:0000/64"
-    GATEWAY="2001:ee0:4f9b:92b0::1"
+# Kiểm tra xem có sự tồn tại của mô-đun IPv6 trong kernel hay không
+if lsmod | grep -q '^ipv6\s'; then
+    echo "IPv6 module is already loaded"
 else
-    IPV6_ADDRESS="2001:ee0:0:$IPC::$IPD:0000/64"
-    GATEWAY="2001:ee0:0:$IPC::1"
+    # Nếu mô-đun IPv6 chưa được tải, hãy tải nó lên
+    modprobe ipv6
+    echo "IPv6 module loaded"
 fi
 
-# Kiểm tra xem tệp cấu hình Netplan có tồn tại không trước khi thay đổi nó
-NETPLAN_PATH="/etc/netplan/99-netcfg-vmware.yaml"  
-if [ -f "$NETPLAN_PATH" ]; then
-    NETPLAN_CONFIG=$(cat "$NETPLAN_PATH")
-    
-    NEW_NETPLAN_CONFIG=$(sed "/gateway4:/i \ \ \ \ \ \ \  - $IPV6_ADDRESS" <<< "$NETPLAN_CONFIG")
-    NEW_NETPLAN_CONFIG=$(sed "/gateway4:.*/a \ \ \ \ \  gateway6: $GATEWAY" <<< "$NEW_NETPLAN_CONFIG")
+# Thiết lập cấu hình mặc định cho IPv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sysctl -w net.ipv6.conf.default.disable_ipv6=0
 
-    echo "$NEW_NETPLAN_CONFIG" > "$NETPLAN_PATH"
-
-    # Áp dụng cấu hình Netplan
-    sudo netplan apply
+# Kiểm tra xem địa chỉ IPv6 đã được cấu hình hay chưa
+if ip -6 address show | grep -q 'inet6'; then
+    echo "IPv6 address already configured"
 else
-    echo "Tệp cấu hình Netplan không tồn tại."
+    # Nếu chưa có địa chỉ IPv6, hãy tạo một địa chỉ IPv6 mới
+    ip -6 address add 2001:ee0:4f9b:92b0::1/64 dev eth0
+    echo "IPv6 address configured"
 fi
 
-# Configure IPv6 settings
-echo "IPV6_ADDR_GEN_MODE=stable-privacy" >> /etc/network/interfaces
-echo "IPV6ADDR=$IPV6_ADDRESS/64" >> /etc/network/interfaces
-echo "IPV6_DEFAULTGW=$GATEWAY" >> /etc/network/interfaces
-
-# Restart networking service
+# Kiểm tra xem đã có gateway IPv6 được cấu hình hay chưa
+if ip -6 route show default | grep -q 'via'; then
+    echo "IPv6 default gateway already configured"
+else
+    # Nếu chưa có gateway IPv6, hãy thêm một gateway IPv6 mới
+    ip -6 route add default via 2001:ee0:4f9b:92b0::1 dev eth0
+    echo "IPv6 default gateway configured"
+fi
 service networking restart
