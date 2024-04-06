7e4326d04176a100032f69f3c25383669acb8fef
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-06 13:01:48 +0700

    Update 5k.sh

--- 5k.sh
+++ 5k.sh
@@ -1,119 +1,110 @@
 #!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
 random() {
-  tr </dev/urandom -dc A-Za-z0-9 | head -c5
-  echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-  ip64() {
-    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-  }
-  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+
 install_3proxy() {
-  echo "installing 3proxy"
-  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-  wget -qO- $URL | bsdtar -xvf-
-  cd 3proxy-3proxy-0.8.6
-  make -f Makefile.Linux
-  mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-  cp src/3proxy /usr/local/etc/3proxy/bin/
-  cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-  chmod +x /etc/init.d/3proxy
-  chkconfig 3proxy on
-  cd $WORKDIR
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cd $WORKDIR
+}
+
+download_proxy() {
+    cd /home/cloudfly
+    curl -F "file=@proxy.txt" https://file.io
 }
 
 gen_3proxy() {
-  cat <<EOF
+    cat <<EOF
 daemon
-maxconn 1000
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
-  cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4}' ${WORKDATA})
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-upload_proxy() {
-  local PASS=$(random)
-  zip --password $PASS proxy.zip proxy.txt
-  URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-
-  echo "Proxy is ready! Format IP:PORT"
-  echo "Download zip archive from: ${URL}"
-  echo "Password: ${PASS}"
-
-}
-
-install_jq() {
-  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-  chmod +x ./jq
-  cp jq /usr/bin
-}
-
-upload_2file() {
-  local PASS=$(random)
-  zip --password $PASS proxy.zip proxy.txt
-  JSON=$(curl -F "file=@proxy.zip" https://file.io)
-  URL=$(echo "$JSON" | jq --raw-output '.link')
-
-  echo "Proxy is ready! Format IP:PORT"
-  echo "Download zip archive from: ${URL}"
-  echo "Password: ${PASS}"
-}
-
 gen_data() {
-  seq $FIRST_PORT $LAST_PORT | while read port; do
-    echo "$IP4/$port/$(gen64 $IP6)"
-  done
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
 gen_iptables() {
-  cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+    cat <<EOF$(awk -F “/” ‘{print “iptables -I INPUT -p tcp –dport “ $2 “  -m state –state NEW -j ACCEPT”}’ ${WORKDATA})
 EOF
 }
 
 gen_ifconfig() {
-  cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+cat <<EOF
+$(awk -F “/” ‘{print “ifconfig eth0 inet6 add “ $3 “/64”}’ ${WORKDATA})
 EOF
 }
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+
+echo “installing apps”
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
+
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
+
+echo “installing apps”
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = /home/cloudfly"
-WORKDIR="/home/cloudfly
-WORKDATA="${WORKDIR}/data.txt"
+echo “working folder = /home/cloudfly”
+WORKDIR=”/home/cloudfly”
+WORKDATA=”${WORKDIR}/data.txt”
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d’:’)
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo “Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}”
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
-
-FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+while :; do
+read -p “Enter FIRST_PORT between 21000 and 61000: “ FIRST_PORT
+[[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo “Enter a valid number”; continue; }
+if ((FIRST_PORT >= 21000 && FIRST_PORT <= 61000)); then
+echo “OK! Valid number”
+break
+else
+echo “Number out of range, try again”
+fi
+done
+LAST_PORT=$(($FIRST_PORT + 750))
+echo “LAST_PORT is $LAST_PORT. Continue…”
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
@@ -125,14 +116,15 @@ gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-service 3proxy start
+ulimit -n 1000048
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
-
+chmod 0755 /etc/rc.local
 bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-# upload_proxy
+echo “Starting Proxy”
+download_proxy
 
-install_jq && upload_2file
+    
