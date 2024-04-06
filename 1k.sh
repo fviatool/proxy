fd23eb74185078b43dfd8a6d48f21702ad550d6f
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-06 13:23:52 +0700

    Update 1k.sh

--- 1k.sh
+++ 1k.sh
@@ -21,9 +21,9 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
     cd $WORKDIR
 }
 download_proxy() {
