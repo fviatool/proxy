aa976a602fde7d99ca95d1709d3d29cde454e8ad
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-09 07:36:11 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,21 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# Lấy địa chỉ IPv6 và gateway từ lệnh ip
-IPV6ADDR=$(ip -6 addr show dev eth0 | grep inet6 | awk '{print $2}' | grep -v '^fe80' | cut -d'/' -f1)
-IPV6_DEFAULTGW=$(ip -6 route show default | awk '/via/ {print $3}')
-
-# Thêm cấu hình IPv6 vào tệp cấu hình của giao diện mạng
-echo "IPV6_FAILURE_FATAL=no
-IPV6_ADDR_GEN_MODE=stable-privacy
-IPV6ADDR=$IPV6ADDR/64
-IPV6_DEFAULTGW=$IPV6_DEFAULTGW" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-
-# Khởi động lại dịch vụ mạng để áp dụng cấu hình mới
-service network restart
-
-# Cài đặt và cấu hình 3proxy
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c8
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
@@ -29,120 +16,117 @@ gen64() {
 }
 
 install_3proxy() {
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,stat}
-    cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ../init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
     cd $WORKDIR
+}
+
+download_proxy() {
+cd /home/cloudfly
+curl -F "file=@proxy.txt" https://file.io
 }
 
 gen_3proxy() {
     cat <<EOF
 daemon
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456 
 flush
-log /dev/null
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong cache\n" \
-"allow "$1"\n" \
-"socks -n -a -s0 -64 -olSO_REUSEADDR,SO_REUSEPORT -ocTCP_TIMESTAMPS,TCP_NODELAY -osTCP_NODELAY -p"$4" -i"$3" -e"$5"\n" \
+$(awk -F "/" '{print "\n" \
+"" $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3":"$4":"$1":"$2}' ${WORKDATA})
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
-}
-
-upload_proxy() {
-    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
 }
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "//$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
     cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport "$4"  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add "$5"/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
-yum -y install gcc net-tools bsdtar zip >/dev/null
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
+
+echo "installing apps"
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-WORKDIR="/home/proxy-installer"
+echo "working folder = /home/cloudfly"
+WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-echo "Your IPv4 address:"
-read IPv4
-echo "Your IPv6 subnet:"
-read IPv6
-
-IP4=${IPv4}
-IP6=${IPv6}
+IP4=192.168.1.123
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create?"
-read COUNT
-
-echo "Port starting proxies:"
-read START_PORT
-
-FIRST_PORT=${START_PORT}
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+while :; do
+  FIRST_PORT=$(($(od -An -N2 -i /dev/urandom) % 80001 + 10000))
+  if [[ $FIRST_PORT =~ ^[0-9]+$ ]] && ((FIRST_PORT >= 10000 && FIRST_PORT <= 80000)); then
+    echo "OK! Valid number"
+    LAST_PORT=$((FIRST_PORT + 9999))
+    echo "LAST_PORT is $LAST_PORT. Continue..."
+    break
+  else
+    echo "Invalid number, try again"
+  fi
+done
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-echo "net.ipv6.conf.eth0.proxy_ndp=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.ip_nonlocal_bind=1" >> /etc/sysctl.conf
-echo "vm.max_map_count=95120" >> /etc/sysctl.conf
-echo "kernel.pid_max=95120" >> /etc/sysctl.conf
-echo "net.ipv4.ip_local_port_range=1024 65000" >> /etc/sysctl.conf
-sudo sysctl -p
-
-chmod -R 777 /usr/local/etc/3proxy/
 
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-
-ulimit -n 999999
-
-service 3proxy start
+ulimit -n 10048
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
 bash /etc/rc.local
 
 gen_proxy_file_for_user
-upload_proxy
+rm -rf /root/3proxy-3proxy-0.8.6
+
+echo "Starting Proxy"
+download_proxy
