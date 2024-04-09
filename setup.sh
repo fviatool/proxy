acd119dfcb9d40f7cd48e1eadcabd278ce8e3bea
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-09 16:45:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
-# Set the PATH to include common command directories
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/count=0
+
+# Path to IP and AWK commands
 cmd_ip="/sbin/ip"
-cmd_awk="/usr/bin/awk"
-cmd_head="/usr/bin/head"
+
+# Configuration directory for Squid
 squid_confd="/etc/squid/conf.d/"
+
+# Network interface
 interface="eth0"
-ipv4Local="192.168.1.9" 
-network=$($cmd_ip -o -f inet6 addr show dev $interface | $cmd_awk '/scope global/ {print $4}' | $cmd_head -c -24)
+
+# Local IPv4 address
+ipv4Local="192.168.1.9"
+
+# Get the global IPv6 address
+ipv6Global=$($cmd_ip -o -f inet6 addr show dev $interface | $cmd_awk '/scope global/ {print $4}' | $cmd_head -c -24)
+
+# Sleep time between iterations
 sleeptime="30s"
+
+# Port range
 port_range_start=8000
 port_range_end=9000
 
-# -----
-# Generate Random Address
-# Thx to Vladislav V. Prodan [https://gist.github.com/click0/939739]
-# -----
+# Function to generate random IPv6 addresses
 GenerateAddress() {
   array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
   a=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
   b=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
   c=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
   d=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
-  echo $network:$a:$b:$c:$d
+  echo $ipv6Global:$a:$b:$c:$d
 }
 
-# -----
-# Run IPv6-Address-Loop
-# -----
-while [ 0=1 ]
-do
+# Run the IPv6 address loop
+while true; do
   # Generate a random number of ports between 1 and 3
   num_ports=$(( (RANDOM % 3) + 1 ))
+
   for ((i=1; i<=$num_ports; i++)); do
     ip=$(GenerateAddress)
     port=$(( (RANDOM % ($port_range_end - $port_range_start + 1)) + $port_range_start ))
-    echo "[+] add ip$count port$i $ip:$port"
+    echo "[+] Adding IP $ip:$port"
 
+    # Add IPv6 address to the interface
     $cmd_ip -6 addr add $ip/64 dev $interface
-    echo "acl ip$count port$i random 1/3
-tcp_outgoing_address $ip ip$count" > $squid_confd"0${count}-random-ip${count}-port${i}.conf"
+
+    # Create Squid configuration for the IP and port
+    echo "acl ip$i random 1/3
+tcp_outgoing_address $ip" > $squid_confd"0${i}-random-ip${i}.conf"
   done
 
-  if [[ $count > 0 ]]; then
-    for ((i=1; i<=$num_ports; i++)); do
-      echo "[-] del ip$count port$i $ip"
-      $cmd_ip -6 addr del $ip/64 dev $interface
-      rm $squid_confd"66-random-ip$count-port$i.conf" > /dev/null 2>&1
-    done
-  fi
-  /bin/systemctl reload squid
-  sleep $sleeptime
+  # Reload Squid configuration
+  systemctl reload squid
 
-  ((count++))
+  # Wait for some time before next iteration
+  sleep $sleeptime
 done
