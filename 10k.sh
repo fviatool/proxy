a7050d3ad901b045a30b18f4a26fd7a65ec0e6a2
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-12 21:44:52 +0700

    Update 10k.sh

--- 10k.sh
+++ 10k.sh
@@ -13,24 +13,35 @@ gen64() {
 	}
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+
+# Hàm kiểm tra và chọn tên giao diện mạng tự động
+auto_detect_interface() {
+    INTERFACE=$(ip -o link show | awk -F': ' '$3 !~ /lo|vir|^[^0-9]/ {print $2; exit}')
+}
+
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.9.4
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | tar -xzvf -
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
-    cd $WORKDIR
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR || return
+}
+
+download_proxy() {
+    cd /home/cloudfly || return
+    curl -F "file=@proxy.txt" https://file.io
 }
 
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 10000
+maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -55,6 +66,7 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "//$IP4/$port/$(gen64 $IP6)"
@@ -63,86 +75,67 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+# Hàm cập nhật thông tin giao diện mạng tự động
+update_network_info() {
+    auto_detect_interface
+}
+
+get_ipv4() {
+    ipv4=$(curl -4 -s icanhazip.com)
+    echo "$ipv4"
+}
+
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
 EOF
-}
 
-rotate_ipv6() {
-  echo "Rotating IPv6 addresses..."
-  
-  # Add your logic to obtain a new IPv6 address
-  new_ipv6=$(get_new_ipv6)
-
-  # Update 3proxy configuration with the new IPv6 address
-  update_3proxy_config "$new_ipv6"
-
-  # Your logic to restart the 3proxy service
-  restart_3proxy
-
-  echo "IPv6 rotation completed."
-}
-
-get_new_ipv6() {
-  # Your logic to obtain a new IPv6 address
-  # For example, generate a random IPv6 address
-  random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
-  echo "$random_ipv6"
-}
-
-restart_3proxy() {
-  # Your logic to restart the 3proxy service
-  service 3proxy restart
-}
-
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
+echo "Dang Tien Hanh Cai Dat Proxy"
 
 install_3proxy
 
-echo "working folder = /home/bc"
-WORKDIR="/home/bc"
+echo "working folder = /home/cloudfly"
+WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -p $WORKDIR && cd $WORKDIR
 
-IP4=$(curl -4 -s icanhazip.com)
+update_network_info
+
+IP4=$(get_ipv4)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-FIRST_PORT=5001
-LAST_PORT=15000
-
+while :; do
+  FIRST_PORT=$(($(od -An -N2 -i /dev/urandom) % 80001 + 10000))
+  if [[ $FIRST_PORT =~ ^[0-9]+$ ]] && ((FIRST_PORT >= 10000 && FIRST_PORT <= 80000)); then
+    echo "OK! Random Ngau Nhien Port"
+    LAST_PORT=$((FIRST_PORT + 9999))
+    echo "LAST_PORT is $LAST_PORT. Tien Hanh Tiep Tuc..."
+    break
+  else
+    echo "Thiet Lap 10k Proxy"
+  fi
+done
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-# Add rotation to rc.local
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
-# Loop to rotate every 10 minutes
-while true; do
-  rotate_ipv6
-  sleep 600  # Sleep for 10 minutes
-done
-
-# Uncomment the line below if you want to start the rotation immediately
-# rotate_ipv6
+bash /etc/rc.local
 
 gen_proxy_file_for_user
-rm -rf /root/3proxy-3proxy-0.9.4
-
+rm -rf /root/3proxy-3proxy-0.8.6
 echo "Starting Proxy"
+download_proxy
