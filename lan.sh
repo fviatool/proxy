dcb0bd677b5c345eaee2d38701a15c1c9745764f
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-14 04:54:56 +0700

    Update lan.sh

--- lan.sh
+++ lan.sh
@@ -20,17 +20,16 @@ auto_detect_interface() {
 }
 
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | tar -xzvf -
-    cd 3proxy-3proxy-0.8.6
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    mkdir -p /usr/local/etc/3proxy/{bin,stat}
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ../init.d/3proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
-    cd $WORKDIR || return
+    cd $WORKDIR
 }
 
 download_proxy() {
