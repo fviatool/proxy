9bb58d5a031d81f57a0c0c4366642ec9665a2951
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-24 11:22:24 +0700

    Update no.sh

--- no.sh
+++ no.sh
@@ -1,7 +1,3 @@
-
-
-Create setup.sh
-8 months ago
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
@@ -19,9 +15,9 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.9.4
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
@@ -34,7 +30,7 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 10000
+maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -59,105 +55,65 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "//$IP4/$port/$(gen64 $IP6)"
     done
 }
 
-gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-rotate_proxy_script() {
-    cat <<EOF
-#!/bin/sh
-service 3proxy restart
-EOF
-}
-
-rotate_ipv6() {
-    echo "Rotating IPv6 addresses..."
-    new_ipv6=$(get_new_ipv6)
-    update_3proxy_config "$new_ipv6"
-    service 3proxy restart
-    echo "3proxy restarted successfully."
-    echo "IPv6 rotation completed."
-}
-
-get_new_ipv6() {
-    random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
-    echo "$random_ipv6"
-}
-
-update_3proxy_config() {
-    new_ipv6=$1
-    sed -i "s/old_ipv6_address/$new_ipv6/" /usr/local/etc/3proxy/3proxy.cfg
-}
-
-add_rotation_cronjob() {
-    echo "*/10 * * * * root ${WORKDIR}/rotate_proxies.sh" >> /etc/crontab
-    echo "Cronjob added for IPv6 rotation every 10 minutes."
-}
-
-# Tự động xoay proxy sau mỗi 10 phút
-(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_proxies.sh") | crontab -
-
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
-
-install_3proxy
-
 echo "working folder = /home/cloudfly"
 WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -p $WORKDIR && cd $_ || exit 1
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal IP = ${IP4}. External subnet for IPv6 = ${IP6}"
 
 while :; do
-    read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
-    [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-    if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
-        echo "OK! Valid number"
-        break
-    else
-        echo "Number out of range, try again"
-    fi
+  read -p "Enter FIRST_PORT between 33000 and 63000: " FIRST_PORT
+  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((FIRST_PORT >= 33000 && FIRST_PORT <= 63000)); then
+    echo "OK! Valid number"
+    break
+  else
+    echo "Number out of range, try again"
+  fi
 done
 
-LAST_PORT=$(($FIRST_PORT + 20000))
-echo "LAST_PORT is $LAST_PORT. Continue..."
-
+while :; do
+  read -p "Enter the number of ports you want to generate: " PORT_COUNT
+  [[ $PORT_COUNT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((PORT_COUNT > 0)); then
+    echo "OK! Valid number"
+    LAST_PORT=$(($FIRST_PORT + $PORT_COUNT - 1))
+    echo "LAST_PORT is $LAST_PORT. Continue..."
+    break
+  else
+    echo "Number must be greater than 0, try again"
+  fi
+done
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x $WORKDIR/boot_*.sh /etc/rc.local
+chmod +x boot_*.sh /etc/rc.local
 
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+# gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-cat >>/etc/rc.local <<EOF
+cat << EOF > /etc/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
-bash /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
 
-gen_proxy_file_for_user
-rm -rf /root/3proxy-3proxy-0.9.4
+sudo /etc/rc.local
 
 echo "Starting Proxy"
