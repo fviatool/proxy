c2c904a430886ecffa08fdc95d332db11cbadb80
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-28 15:56:07 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,6 +1,72 @@
 #!/bin/bash
 
-# Kiểm tra xem có sự tồn tại của mô-đun IPv6 trong kernel hay không
+# Kiểm tra card mạng eth0
+if ip link show eth0 &> /dev/null; then
+    echo "Card mang eth0 !"
+    
+    # Thiết lập cấu hình mạng cho eth0
+    cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
+TYPE=Ethernet
+NAME=eth0
+DEVICE=eth0
+ONBOOT=yes
+BOOTPROTO=dhcp
+IPV6_INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+IPV6_ADDR_GEN_MODE=eui64
+IPV6ADDR=2001:ee0:4f9b:92b0::75:0000/64
+IPV6_DEFAULTGW=2001:ee0:4f9b:92b0::1
+EOF
+
+    # Kiểm tra kết nối IPv6
+    if ip -6 route get 2001:ee0:4f9b:92b0::8888 &> /dev/null; then
+        echo "Ket Noi ipv6 eth0 start."
+    else
+        echo "Lỗi: Kết nối IPv6 cho eth0 không hoạt động."
+    fi
+
+    # Cấp quyền cho địa chỉ IPv4 của eth0
+    firewall-cmd --zone=public --add-source=192.168.1.17 --permanent
+    firewall-cmd --reload
+
+# Kiểm tra card mạng ens33
+elif ip link show ens33 &> /dev/null; then
+    echo "Card mang ens33 !"
+    
+    # Thiết lập cấu hình mạng cho ens33
+    cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-ens33
+TYPE=Ethernet
+NAME=ens33
+DEVICE=ens33
+ONBOOT=yes
+BOOTPROTO=dhcp
+IPV6_INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+IPV6_ADDR_GEN_MODE=eui64
+IPV6ADDR=2001:ee0:4f9b:92b0::75:0000/64
+IPV6_DEFAULTGW=2001:ee0:4f9b:92b0::1
+EOF
+
+    # Kiểm tra kết nối IPv6
+    if ip -6 route get 2001:ee0:4f9b:92b0::8888 &> /dev/null; then
+        echo "Ket Noi ipv6 ens33 Start"
+    else
+        echo "Lỗi: Kết nối IPv6 cho ens33 không hoạt động."
+    fi
+
+    # Cấp quyền cho địa chỉ IPv4 của ens33
+    firewall-cmd --zone=public --add-source=192.168.1.17 --permanent
+    firewall-cmd --reload 
+
+else
+    echo "Không tìm thấy card mạng eth0 hoặc ens33."
+fi
+
+# Kiểm tra mô-đun IPv6
 if lsmod | grep -q '^ipv6\s'; then
     echo "IPv6 module is already loaded"
 else
@@ -30,4 +96,20 @@ else
     ip -6 route add default via 2001:ee0:4f9b:92b0::1 dev eth0
     echo "IPv6 default gateway configured"
 fi
-service networking restart
+
+# Khởi động lại dịch vụ mạng
+sudo systemctl restart network
+
+# Ping google.com bằng IPv6
+ping_google6() {
+    ping6 -c 3 google.com
+}
+
+# Đếm tổng số địa chỉ IPv6 trên máy chủ
+ip -6 addr | grep inet6 | wc -l
+
+# Kiểm tra kết nối IPv6 bằng cách ping Google
+ping_google6
+ifconfig
+
+reboot
