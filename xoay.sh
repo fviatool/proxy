f2a5e6dc04da2083fd7d80bdde3357cc7239c3cf
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-16 09:36:42 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -7,7 +7,6 @@ random() {
 }
 
 # Hàm gen64 tạo địa chỉ IPv6 ngẫu nhiên
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -19,79 +18,6 @@ gen64() {
 auto_detect_interface() {
     INTERFACE=$(ip -o link show | awk -F': ' '$3 !~ /lo|vir|^[^0-9]/ {print $2; exit}')
 }
-
-# Get IPv6 address
-ipv6_address=$(ip addr show eth0 | awk '/inet6/{print $2}' | grep -v '^fe80' | head -n1)
-
-# Kiểm tra xem có nhận được địa chỉ IPv6 không
-if [ -n "$ipv6_address" ]; then
-    echo "IPv6 address obtained: $ipv6_address"
-
-    # Khai báo mảng kết hợp để lưu trữ địa chỉ IPv6 và cổng cổng mặc định
-    declare -A ipv6_addresses=(
-        [4]="2001:ee0:4f9b::$IPD:0000/64"
-        [5]="2001:ee0:4f9b::$IPD:0000/64"
-        [244]="2001:ee0:4f9b::$IPD:0000/64"
-        ["default"]="2001:ee0:4f9b::$IPC::$IPD:0000/64"
-    )
-
-    declare -A gateways=(
-        [4]="2001:ee0:4f9b:$IPC::1"
-        [5]="2001:ee0:4f9b:$IPC::1"
-        [244]="2001:ee0:4f9b:$IPC::1"
-        ["default"]="2001:ee0:4f9b:$IPC::1"
-    )
-
-    # Lấy ra các octet thứ ba và thứ tư của IPv4
-    IPC=$(echo "$ipv6_address" | cut -d":" -f5)
-    IPD=$(echo "$ipv6_address" | cut -d":" -f6)
-
-    # Đặt địa chỉ IPv6 và cổng mặc định dựa trên octet thứ ba của IPv4
-    IPV6_ADDRESS="${ipv6_addresses[$IPC]}"
-    GATEWAY="${gateways[$IPC]}"
-
-    # Kiểm tra xem giao diện có sẵn không
-    INTERFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | head -n1)
-
-    if [ -n "$INTERFACE" ]; then
-        echo "Configuring interface: $INTERFACE"
-
-        # Cấu hình IPv6
-        echo "IPV6_ADDR_GEN_MODE=stable-privacy" >> /etc/network/interfaces
-        echo "IPV6ADDR=$ipv6_address/64" >> /etc/network/interfaces
-        echo "IPV6_DEFAULTGW=$GATEWAY" >> /etc/network/interfaces
-
-        # Khởi động lại dịch vụ mạng
-        service networking restart
-        systemctl restart NetworkManager.service
-        ifconfig "$INTERFACE"
-        echo "Done!"
-    else
-        echo "No network interface available."
-    fi
-else
-    echo "No IPv6 address obtained."
-fi
-
-install_3proxy() {
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,stat}
-    cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ../init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
-    cd $WORKDIR
-}
-
-WORKDIR="/home/cloudfly"
-WORKDATA="${WORKDIR}/data.txt"
-MAXCOUNT=2222
-IFCFG="eth0"
-FIRST_PORT=10000
-LAST_PORT=10500
 
 # Hàm rotate_ipv6 xoay địa chỉ IPv6
 rotate_ipv6() {
@@ -166,6 +92,26 @@ download_proxy() {
     curl -F "file=@proxy.txt" https://file.io
 }
 
+# Hàm cài đặt 3proxy
+install_3proxy() {
+    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.4
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,stat}
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ../init.d/3proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
+}
+
+# Thiết lập biến môi trường
+WORKDIR="/home/cloudfly"
+WORKDATA="${WORKDIR}/data.txt"
+MAXCOUNT=2222
+IFCFG="eth0"
+
 # Bước cài đặt
 echo "Installing necessary packages..."
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
@@ -175,9 +121,10 @@ rm -rf /root/3proxy-0.9.4
 echo "Working folder: $WORKDIR"
 mkdir -p "$WORKDIR" && cd "$WORKDIR" || exit
 
-# Install 3proxy
+# Cài đặt 3proxy
 install_3proxy
 
+# Tạo cấu hình 3proxy
 gen_3proxy > "/usr/local/etc/3proxy/3proxy.cfg"
 
 # Tạo dữ liệu cho các địa chỉ IPv6
@@ -202,7 +149,10 @@ echo "Number of current IPv6 addresses:"
 ip -6 addr | grep inet6 | wc -l
 download_proxy
 
-echo "3proxy dang xoay tu dong..."
+echo "3proxy đang xoay tự động..."
+
+# Khởi động xoay IPv6 tự động
+rotate_ipv6 &
 
 # Function to rotate IPv6 addresses
 rotate_ipv6() {
@@ -224,8 +174,3 @@ rotate_ipv6() {
         sleep 300  # Chờ 5 phút trước khi cập nhật lại
     done
 }
-
-# Khởi động xoay IPv6 tự động
-rotate_auto_ipv6 &
-# Chạy hàm rotate_ipv6 để cài đặt xoay IPv6
-rotate_ipv6
