89715332d35091ecb7732cdf5967ebb3e7cc655a
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-16 13:20:27 +0700

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,5 +1,62 @@
-#!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#!/bin/bash
+
+auto_detect_interface() {
+    INTERFACE=$(ip -o link show | awk -F': ' '$3 !~ /lo|vir|^[^0-9]/ {print $2; exit}')
+}
+
+# Get IPv6 address
+ipv6_address=$(ip addr show eth0 | awk '/inet6/{print $2}' | grep -v '^fe80' | head -n1)
+
+# Kiểm tra xem có nhận được địa chỉ IPv6 không
+if [ -n "$ipv6_address" ]; then
+    echo "IPv6 address obtained: $ipv6_address"
+
+    # Khai báo mảng kết hợp để lưu trữ địa chỉ IPv6 và cổng cổng mặc định
+    declare -A ipv6_addresses=(
+        [4]="2001:ee0:4f9b::$IPD:0000/64"
+        [5]="2001:ee0:4f9b::$IPD:0000/64"
+        [244]="2001:ee0:4f9b::$IPD:0000/64"
+        ["default"]="2001:ee0:4f9b::$IPC::$IPD:0000/64"
+    )
+
+    declare -A gateways=(
+        [4]="2001:ee0:4f9b:$IPC::1"
+        [5]="2001:ee0:4f9b:$IPC::1"
+        [244]="2001:ee0:4f9b:$IPC::1"
+        ["default"]="2001:ee0:4f9b:$IPC::1"
+    )
+
+    # Lấy ra các octet thứ ba và thứ tư của IPv4
+    IPC=$(echo "$ipv6_address" | cut -d":" -f5)
+    IPD=$(echo "$ipv6_address" | cut -d":" -f6)
+
+    # Đặt địa chỉ IPv6 và cổng mặc định dựa trên octet thứ ba của IPv4
+    IPV6_ADDRESS="${ipv6_addresses[$IPC]}"
+    GATEWAY="${gateways[$IPC]}"
+
+    # Kiểm tra xem giao diện có sẵn không
+    INTERFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | head -n1)
+
+    if [ -n "$INTERFACE" ]; then
+        echo "Configuring interface: $INTERFACE"
+
+        # Cấu hình IPv6
+        echo "IPV6_ADDR_GEN_MODE=stable-privacy" >> /etc/network/interfaces
+        echo "IPV6ADDR=$ipv6_address/64" >> /etc/network/interfaces
+        echo "IPV6_DEFAULTGW=$GATEWAY" >> /etc/network/interfaces
+
+        # Khởi động lại dịch vụ mạng
+        service networking restart
+        systemctl restart NetworkManager.service
+        ifconfig "$INTERFACE"
+        echo "Done!"
+    else
+        echo "No network interface available."
+    fi
+else
+    echo "No IPv6 address obtained."
+fi
+
 
 random() {
 	tr </dev/urandom -dc A-Za-z0-9 | head -c5
