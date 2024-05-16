b2289a4207aba09ac391e1dfd0b0f4017638b20f
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-16 10:59:07 +0700

    Update o.sh

--- o.sh
+++ o.sh
@@ -1,176 +1,66 @@
-#!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#!/bin/bash
 
-# Hàm tạo chuỗi ngẫu nhiên
-random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+# Function to generate a random IPv6 address segment
+ip64() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 }
 
-# Hàm tạo IPv6
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+# Function to generate a full IPv6 address
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-# Cài đặt 3proxy
-install_3proxy() {
-    echo "Đang cài đặt 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cd $WORKDIR
+# Function to generate ifconfig commands for IPv4 and save to file
+gen_ifconfig_ipv4() {
+    local port=$1
+    local ipv4=$2
+    echo "ifconfig eth0 inet add $ipv4 port $port"
 }
 
-# Tải proxy từ file.io
-download_proxy() {
-    cd /home/cloudfly || return
-    curl -F "file=@proxy.txt" https://file.io
+# Function to generate ifconfig commands for IPv6 and save to file
+gen_ifconfig_ipv6() {
+    local port=$1
+    local ipv6=$2
+    echo "ifconfig eth0 inet6 add $ipv6/64 port $port"
 }
 
-# Tạo cấu hình 3proxy
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456 
-flush
-
-$(awk -F "/" '{print "\n" \
-"" $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
-}
-
-# Hàm tạo danh sách IPv6
-gen_ipv6() {
-    while IFS= read -r line; do
-        # Tách dòng thành các phần IPv4, cổng và IPv6
-        IFS="/" read -r _ _ ip6 _ <<< "$line"
-
-        # Sử dụng chỉ IPv6 ở đây
-        echo "IPv6: $ip6"
-    done < "$WORKDATA/ipv6.txt"
-}
-
-# Hàm tạo tệp proxy cho người dùng
-gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA}/data.txt)
-$(awk -F'/' '{print $3}' "$WORKDATA/ipv6.txt")
-EOF
-}
-
-# Hàm tạo dữ liệu cho proxy
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//$IP4/$port/$(gen64 $IP6)"
+# Generate IPv6 addresses and save to file
+generate_ipv6_addresses() {
+    local ipv6_prefix="$1"
+    local port_count=$2
+    for ((i = 0; i < $port_count; i++)); do
+        local ipv6_address="$ipv6_prefix:$(ip64):$(ip64):$(ip64):$(ip64)"
+        echo "$ipv6_address"
     done
 }
 
-# Hàm tạo rules iptables
-gen_iptables() {
-    cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
+# Main script
+WORKDIR="home/cloudfly"  # Change directory path if needed
+mkdir -p "$WORKDIR" && cd "$WORKDIR" || exit
+PORT_COUNT=100
+IPv4="192.168.1.151"  # Change to your desired IPv4 address
+IPv6_PREFIX="2001:db8:abcd"  # Change to your desired IPv6 prefix
 
-# Hàm tạo lệnh ifconfig
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
+# Array containing hex values from 0 to f
+array=(0 1 2 3 4 5 6 7 8 9 a b c d e f)
 
-# Tạo file rc.local
-cat << EOF > /etc/rc.d/rc.local
-#!/bin/bash
-touch /var/lock/subsys/local
-EOF
+# Generate IPv6 addresses and save to file
+generate_ipv6_addresses "$IPv6_PREFIX" "$PORT_COUNT" > ipv6_addresses.txt
 
-# Cài đặt các ứng dụng cần thiết
-echo "Đang cài đặt ứng dụng"
-yum -y install wget gcc net-tools bsdtar zip >/dev/null
-
-# Cài đặt 3proxy
-install_3proxy
-
-# Thư mục làm việc
-echo "Thư mục làm việc = /home/cloudfly"
-WORKDIR="/home/cloudfly"
-WORKDATA="${WORKDIR}"
-mkdir -p $WORKDIR && cd $
-WORKDIR || exit
-
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-
-echo "Địa chỉ IP nội bộ = ${IP4}. Dải ngoại vi cho IPv6 = ${IP6}"
-
-while :; do
-  read -p "Nhập Cổng: " FIRST_PORT
-  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 20000)); then
-    echo "OK! Valid number"
-    break
-  else
-    echo "Number out of range, try again"
-  fi
+# Generate ifconfig commands for IPv4 and save to file
+port=10000  # Starting port
+for ((i = 0; i < $PORT_COUNT; i++)); do
+    gen_ifconfig_ipv4 "$port" "$IPv4" >> boot_ifconfig.sh
+    ((port++))
 done
 
-LAST_PORT=$(($FIRST_PORT + 500))
-echo "LAST_PORT is $LAST_PORT. Continue..."
+# Generate ifconfig commands for IPv6 and save to file
+port=10000  # Starting port
+while IFS= read -r ipv6_address; do
+    gen_ifconfig_ipv6 "$port" "$ipv6_address" >> boot_ifconfig.sh
+    ((port++))
+done < ipv6_addresses.txt
 
-# Tạo dữ liệu cho proxy
-gen_data > $WORKDATA/data.txt
-
-# Tạo rules iptables
-gen_iptables > $WORKDIR/boot_iptables.sh
-
-# Tạo lệnh ifconfig
-gen_ifconfig > $WORKDIR/boot_ifconfig.sh
-
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
-
-# Tạo cấu hình 3proxy
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-# Thêm lệnh khởi động vào rc.local
-cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-EOF
-
-bash /etc/rc.local
-
-# Tạo file proxy cho người dùng
-gen_proxy_file_for_user
-
-# Dọn dẹp
-rm -rf /root/3proxy-3proxy-0.8.6
-
-# Khởi động proxy
-echo "Bắt đầu Proxy"
-echo "Số lượng địa chỉ IPv6 hiện tại:"
-ip -6 addr | grep inet6 | wc -l
-
-# Tải proxy
-download_proxy
-
+# Grant permission and execute boot_ifconfig.sh
+chmod +x boot_ifconfig.sh
+./boot_ifconfig.sh
