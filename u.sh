71d4f652759299a88e03eab19bdd75cb8e38da74
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-16 12:16:41 +0700

    Update u.sh

--- u.sh
+++ u.sh
@@ -96,10 +96,8 @@ yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = /home/cloudfly"
-WORKDIR="/home/cloudfly"
-WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+echo "Working folder: $WORKDIR"
+mkdir -p "$WORKDIR" && cd "$WORKDIR" || exit
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
