884a38b252a6d83dd18b408a231fbdd6666ffc92
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-16 12:15:00 +0700

    Update u.sh

--- u.sh
+++ u.sh
@@ -99,7 +99,7 @@ install_3proxy
 echo "working folder = /home/cloudfly"
 WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_ || exit
+mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
