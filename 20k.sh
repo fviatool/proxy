f14db3f871b3f2a77aec60d98eaa9c4aa715cc96
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-21 18:24:33 +0700

    Update 20k.sh

--- 20k.sh
+++ 20k.sh
@@ -99,7 +99,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=20000
-LAST_PORT=200100
+LAST_PORT=20100
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
