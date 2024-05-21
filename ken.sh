47112417641d7c6459dba153d228463640cc3855
FVIA <130783224+fviatool@users.noreply.github.com>
2024-05-21 20:43:24 +0700

    Update ken.sh

--- ken.sh
+++ ken.sh
@@ -61,7 +61,7 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
